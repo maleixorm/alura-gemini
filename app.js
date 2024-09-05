5480caaad6220fd9d7cabd8a266fc1a288d85eeb
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-09-05 11:37:59 -0300

    Criando a  função pesquisar

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('Rebeca Andrade');
+function pesquisar() {
+    let section = document.getElementById("resultados-pesquisa");
+
+    let resultados = "";
+
+    // Para cada dado dentro da lista de dados
+    for (let atleta of atletas) {
+        resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${atleta.nome}</a>
+                </h2>
+                <p class="descricao-meta">${atleta.descricao}</p>
+                <a href=${atleta.link} target="_blank">Mais informações</a>
+            </div>
+    `;    
+    }
+
+    section.innerHTML = resultados;
+}
