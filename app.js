e1ba303463dfdfe068463baa3252998eb26df1f8
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-09-06 11:55:08 -0300

    Modificando a função para retornar diferentes valores pesquisados

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 function pesquisar() {
     let section = document.getElementById("resultados-pesquisa");
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+    if (campoPesquisa == "") {
+        section.innerHTML = 'O campo de pesquisa está vazio.';
+        return; 
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase();
 
     let resultados = "";
+    let nome = "";
+    let descricao ="";
+    let tags = "";
 
-    // Para cada dado dentro da lista de dados
+    // Itera sobre cada dado dentro da lista de dados
     for (let atleta of atletas) {
-        resultados += `
+        nome = atleta.nome.toLowerCase();
+        descricao = atleta.descricao.toLowerCase();
+        tags = atleta.tags.toLowerCase();
+        if (nome.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            resultados += `
             <div class="item-resultado">
                 <h2>
                     <a href="#" target="_blank">${atleta.nome}</a>
@@ -13,7 +28,12 @@ function pesquisar() {
                 <p class="descricao-meta">${atleta.descricao}</p>
                 <a href=${atleta.link} target="_blank">Mais informações</a>
             </div>
-    `;    
+        `;   
+        } 
+    }
+
+    if (!resultados) {
+        resultados = 'Nenhum resultado foi encontrado.';
     }
 
     section.innerHTML = resultados;
