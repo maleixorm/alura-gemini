e7d3bd2c1a30aeb264cff82d5e0ff37cf9d11680
Marcos Marques <67377101+maleixorm@users.noreply.github.com>
2024-09-06 11:54:28 -0300

    Adicionando o campo tags nos dados

--- dados.js
+++ dados.js
@@ -2,16 +2,19 @@ let atletas = [
     {
         nome:"Rebeca Andrade",
         descricao:"Rebeca Andrade é uma das maiores ginastas brasileiras de todos os tempos. Com sua força, elegância e determinação, ela conquistou o coração dos brasileiros e se tornou um verdadeiro ícone do esporte nacional.",
-        link:"https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+        link:"https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags:"ginástica ginastica artística artistica"
     }, 
     {
         nome:"Rayssa Leal",
         descricao:"Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
-        link:"https://pt.wikipedia.org/wiki/Rayssa_Leal"
+        link:"https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags:"skate sk8 street"
     }, 
     {
         nome:"Beatriz Souza",
         descricao:"Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos, trazendo orgulho para o país. Sua força e técnica a colocaram entre as melhores do mundo.",
-        link:"https://pt.wikipedia.org/wiki/Beatriz_Souza"
+        link:"https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags:"judô judo judoka luta"
     }
 ];
